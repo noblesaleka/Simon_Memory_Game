4f245245b118da68ce4dd73c4c0a9074e877ac0b
Ubuntu <ubuntu@ip-172-31-84-92.ec2.internal>
2019-09-23 03:44:51 +0000

    compare user's play and computer's play

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,7 @@
 let randomArray = []; //random array 
 var compOrder = []; // combination of lights played by computer
 let playerOrder = []; //order of player input
-var id, color, level = 0, j = 0, i, k;
+var id, color, level = 0, j = 0, i, k, m ;
 var sound = [
     "assets/audio/Baby Sneeze-SoundBible.com-431839106.mp3",
     "assets/audio/Banana Peel Slip-SoundBible.com-580403617.mp3",
@@ -63,17 +63,8 @@ function newgame() {
     level++;
     levelCounter.innerHTML = "<h3>" + level + "</h3>";
     console.log("You are on level " + level);
-    
-    for (k=0; k<level; k++) {
     iterate();
-    }
-    $("h1").css("color", "blue");
-    
-
-   
-   
-   
-    }
+       }
     
     
     
@@ -92,51 +83,107 @@ function getRandomNumber() {
 
 
     
-//iterate through randomArray, if user is on level 1, the iteration should occur once.
-//if user is on level 3, the iteration should occur 3 times, etc
-function iterate() {
-    if (randomArray[j] == 0) {
-        //style box like this then timeout
-        $("#yellow").addClass("white");
-        compOrder.push(0);
+//iterate through randomArray
+      function iterate() {
+          m = setInterval( function() {
+            if (randomArray[j] == 0) {
+                //style box like this then timeout
+                $("#yellow").addClass("white");
+                compOrder.push(0);
+                
+                    setTimeout(function() {
+                        $("#yellow").removeClass("white"); 
+                    }, 1000)
+            }
+            
+            
+            
+            else if (randomArray[j] == 1) {
+                 $("#green").addClass("white");
+                compOrder.push(0);compOrder.push(1);
+                
+                 setTimeout(function() {
+                        $("#green").removeClass("white"); 
+                    }, 1000)
+            }
+            
+            else if (randomArray[j] == 2) {
+                 $("#red").addClass("white");
+                 setTimeout(function() {
+                        $("#red").removeClass("white"); 
+                    }, 1000)
+                
+                compOrder.push(2);
+            }
         
-            setTimeout(function() {
-                $("#yellow").removeClass("white"); 
-            }, 1000)
-    }
+            else if (randomArray[j] == 3) {
+                $("#blue").addClass("white");
+                setTimeout(function() {
+                        $("#blue").removeClass("white"); 
+                    }, 1000)
+               
+                compOrder.push(3);
+            }
+            
+            j++;
+            console.log(compOrder)
+            
+            if (j=>level) {
+                clearInterval(m)
+            }
+        }, 1000)
+      }
     
-    
-    
-    else if (randomArray[j] == 1) {
-         $("#green").addClass("white");
-        compOrder.push(0);compOrder.push(1);
-        
-         setTimeout(function() {
-                $("#green").removeClass("white"); 
-            }, 1000)
-    }
-    
-    else if (randomArray[j] == 2) {
-         $("#red").addClass("white");
-         setTimeout(function() {
-                $("#red").removeClass("white"); 
-            }, 1000)
-        
-        compOrder.push(2);
-    }
+//user play, each button click will add a number to the playerOrder array
+$("#yellow").on("click", function(){
+    playerOrder.push(0);
+    $("#yellow").addClass("white");
+               setTimeout(function() {
+                        $("#yellow").removeClass("white"); 
+                    }, 1000)
+    compare();
+})
 
-    else if (randomArray[j] == 3) {
-        $("#blue").addClass("white");
-        setTimeout(function() {
-                $("#blue").removeClass("white"); 
-            }, 1000)
-       
-        compOrder.push(3);
+$("#green").on("click", function(){
+    playerOrder.push(1);
+    $("#green").addClass("white");
+               setTimeout(function() {
+                        $("#green").removeClass("white"); 
+                    }, 1000)
+    compare();
+})
+
+$("#red").on("click", function(){
+    playerOrder.push(2);
+    $("#red").addClass("white");
+               setTimeout(function() {
+                        $("#red").removeClass("white"); 
+                    }, 1000)
+    compare();
+})
+
+$("#blue").on("click", function(){
+    playerOrder.push(3);
+    $("#blue").addClass("white");
+               setTimeout(function() {
+                        $("#blue").removeClass("white"); 
+                    }, 1000)
+    compare();
+})
+
+//compare buttons user clicked and computer play
+//only compare if user has clicked the same number of computer plays
+function compare() {
+    console.log("This is computer's play" + compOrder);
+    console.log("This is user's play" + playerOrder);
+    if (compOrder.length == playerOrder.length) {
+        
+        //join values in computer's array 
+        //join values in user's array
+        //if both numbers are the same, user has passed level
+        if (compOrder.join == playerOrder.join) {
+            console.log("you pass")
+        }
+        
     }
-    
-    j++;
-    console.log(compOrder)
 }
-    
-    
-
